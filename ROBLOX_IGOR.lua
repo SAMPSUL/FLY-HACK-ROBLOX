2f324c89fd06a99526ed354d036d938b05b91dc7
SAMPSUL <vikmansamuel@gmail.com>
2025-08-20 06:23:08 +0300

    Update ROBLOX_IGOR.lua

--- ROBLOX_IGOR.lua
+++ ROBLOX_IGOR.lua
@@ -10,7 +10,7 @@ local camera = workspace.CurrentCamera
 local egorOn = true
 local normalSpeed = 16
 local normalJump = 50
-local egorSpeed = 100
+local egorSpeed = 2
 local egorJump = 150
 
 humanoid.WalkSpeed = egorSpeed
@@ -21,7 +21,7 @@ local walkAnim = Instance.new("Animation")
 walkAnim.AnimationId = "rbxassetid://180426354" 
 local walkTrack = animator:LoadAnimation(walkAnim)
 walkTrack:Play()
-walkTrack:AdjustSpeed(3)
+walkTrack:AdjustSpeed(5)
 
 local jumpAnim = Instance.new("Animation")
 jumpAnim.AnimationId = "rbxassetid://125750702" 
