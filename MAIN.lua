5df1c16219269f3da709b156e0cf76ac7b35e574
SAMPSUL <vikmansamuel@gmail.com>
2025-08-23 07:55:50 +0300

    Update MAIN.lua

--- MAIN.lua
+++ MAIN.lua
@@ -35,7 +35,7 @@ title.Parent = frame
 local buttonsInfo = {
     {name="ROBLOX_IGOR", url="https://raw.githubusercontent.com/SAMPSUL/SAMPSULS-MOD-MENU-ROBLOX/refs/heads/main/HACKS/ROBLOX_IGOR%20(NOT%20WORKING%20GOOD).LUA"},
     {name="FLY", url="https://raw.githubusercontent.com/SAMPSUL/SAMPSULS-MOD-MENU-ROBLOX/refs/heads/main/HACKS/FLY.lua"},
-    {name="FLY + SPIN", url="https://raw.githubusercontent.com/SAMPSUL/SAMPSULS-MOD-MENU-ROBLOX/refs/heads/main/FLY%20SPIN.lua"},
+    {name="FLY + SPIN", url="https://raw.githubusercontent.com/SAMPSUL/SAMPSULS-MOD-MENU-ROBLOX/refs/heads/main/HACKS/FLY%20SPIN.lua"},
     {name="NO CLIP", url="https://raw.githubusercontent.com/SAMPSUL/FLY-HACK-ROBLOX/refs/heads/main/NOCLIP.lua"},
     {name="MAX HP", url="https://raw.githubusercontent.com/SAMPSUL/FLY-HACK-ROBLOX/refs/heads/main/MAXHP.lua"},
     {name="NOT WORKING", url="https://raw.githubusercontent.com/USERNAME/REPO/main/script6.lua"},
